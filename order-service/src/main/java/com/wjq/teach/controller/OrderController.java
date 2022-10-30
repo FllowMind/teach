91c21fedd103740df43c7ac67392302c9f83798b
FllowMind <1095682334@qq.com>
2022-10-30 23:04:07 +0800

    提交nacos  动态刷新配置

--- order-service/src/main/java/com/wjq/teach/controller/OrderController.java
+++ order-service/src/main/java/com/wjq/teach/controller/OrderController.java
@@ -3,14 +3,20 @@ package com.wjq.teach.controller;
 import com.wjq.teach.domain.Video;
 import com.wjq.teach.service.VideoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RequestMapping("api/v1/order")
 @RestController
 public class OrderController {
+
+    @Value("${value.test}")
+    private String testValue;
 
     @Autowired
     private VideoService videoService;
@@ -19,5 +25,12 @@ public class OrderController {
     public Video findVideoById(@RequestParam("id") int id) {
         return videoService.findById(id);
     }
+
+    @GetMapping("testNacosConfig")
+    public String testNacosConfig() {
+        return testValue;
+    }
+
+
 
 }
